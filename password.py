fd69459b361d65f380da953a446524e8a03832f7
dainalkai <120482248+dainalkai@users.noreply.github.com>
2022-12-14 18:44:26 +0800

    Add files via upload

--- password.py
+++ password.py
@@ -1,14 +1,16 @@
 ps = 'a123456'
 time = 1
-while time<=3:
-	tps = input('請輸入密碼')
+while time <= 3:
+	tps = input('請輸入密碼:')
 	if tps == ps:
 		print('登入成功')
 		break
 	else:
-		print('密碼錯誤! 還有', 3-time , '次機會')
-		time +=	1
-		if time == 4:
+		if time == 3:
 			print('登入失敗!!')
+			break
+		else:	
+			print('密碼錯誤! 還有', 3-time , '次機會')
+			time +=	1
 		
 
