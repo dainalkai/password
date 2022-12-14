c18b9d52472235a694d930e4125d0c92e974ff4d
dainalkai <120482248+dainalkai@users.noreply.github.com>
2022-12-14 18:30:18 +0800

    Add files via upload

---
+++ password.py
@@ -0,0 +1,14 @@
+ps = 'a123456'
+time = 1
+while time<=3:
+	tps = input('請輸入密碼')
+	if tps == ps:
+		print('登入成功')
+		break
+	else:
+		print('密碼錯誤! 還有', 3-time , '次機會')
+		time +=	1
+		if time == 4:
+			print('登入失敗!!')
+		
+
